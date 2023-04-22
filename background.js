b2ee996475f4261caadf4832f20921bc6a0d8e41
Anan Wang <mengan.wang@gmail.com>
2023-04-22 13:55:14 -0700

    ok

--- background.js
+++ background.js
@@ -1,32 +1,59 @@
-// on activated, listen when on youtubecom
-// when the counter reaches 20 minutes, change tabs/create a new tab
-
-var timer = 60*20; // 20 minutes
-var duration = 60*20;
-
-var breakTimer = 60*5;
-var breakDuration = 60*5;
-
-var onBreak = 0;
-
 chrome.runtime.onInstalled.addListener(({reason}) => {
   if (reason === 'install') {
     chrome.tabs.create({
       url: "hello.html"
     });
 
-    //
-    chrome.history.search({text: '', maxResults: 10}, function(data) {
+    var d = new Date();
+    d.setHours(0,0,0,0);
+    chrome.storage.local.set({ "refreshDate": d });
+
+    //on first open send the entire history, and then afterwards only send every 24 hours
+    chrome.history.search({text: '', maxResults: 100000, startTime: 0}, function(data) {
+      var jsonData = {};
+      var urlArray = [];
+
       data.forEach(function(page) {
-          console.log(page.url);
+          // console.log(page.url);
+          urlArray.push(page.url);
       });
+
+      jsonData["history"] = urlArray;
+      console.log(jsonData);
     });
   }
 });
 
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  dayChange();
+}); 
+
+//send a request every new day to get all the last info
+async function dayChange() {
+  var today = new Date();
+  var lastCheck = await chrome.storage.local.get("refreshDate").refreshDate;
+
+  if (lastCheck === undefined) {
+    lastCheck = new Date();
+  }
+
+  // console.log(lastCheck);
+
+  if (today.getDate() != lastCheck.getDate()) {
+    lastCheck.setHours(0,0,0,0);
+    chrome.storage.local.set({ refreshDate: lastCheck });
+
+    chrome.history.search({text: '', maxResults: 10000, startTime: lastCheck.getTime()}, function(data) {
+      var jsonData = {};
+      var urlArray = [];
+  
+      data.forEach(function(page) {
+          // console.log(page.url);
+          urlArray.push(page.url);
+      });
+  
+      jsonData["history"] = urlArray;
+      console.log(jsonData);
+    });
+  }
 }
